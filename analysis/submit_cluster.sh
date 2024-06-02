4cf2fa2dfc28edac5fb374891e095e4c90a0e837
rasi <rasi1983@gmail.com>
2024-06-02 09:24:22 -0700

    add envmodules and latency wait for snakemake

--- analysis/submit_cluster.sh
+++ analysis/submit_cluster.sh
@@ -1,8 +1,10 @@
 snakemake \
     --jobs 999 \
+    --latency-wait 30 \
     --cluster-config cluster.yaml \
     --cluster "sbatch -n {cluster.n}  -t {cluster.time}" \
     --use-singularity \
+    --use-envmodules \
     --singularity-args "--bind /fh --bind /hpc" \
     --cores=all \
     -p $@
