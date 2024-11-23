6784e4d5c53089d4450f7e1321943c1d74483e99
rasi <rasi1983@gmail.com>
2024-11-23 14:40:23 -0800

    Update submit cluster script

--- analysis/submit_cluster.sh
+++ analysis/submit_cluster.sh
@@ -2,7 +2,7 @@ snakemake \
     --jobs 999 \
     --cluster-config cluster.yaml \
     --cluster "sbatch -J {cluster.name} -n {cluster.cores}  -t {cluster.time} --mem {cluster.mem} -N {cluster.nodes}" \
-    --use-singularity \
+    --use-singularity --latency-wait 10 --retries 3 \
     --singularity-args "--bind /hpc --bind /fh" \
     --cores=all \
     -p $@
